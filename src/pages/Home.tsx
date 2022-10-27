64fcbdc9dfc3a6a9c38f9cfe189343eb57975a95
Big One <sponjjanc@naver.com>
2022-10-27 23:07:45 +0900

    init. 폴더구조 셋팅
    Signed-off-by: Big One <sponjjanc@naver.com>

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text>홈 페이지!!!</Text>
+    </View>
+  );
+}
+
+export default Home;
---
+++ src/pages/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Search() {
+  return (
+    <View>
+      <Text>Search Page.</Text>
+    </View>
+  );
+}
+
+export default Search;
